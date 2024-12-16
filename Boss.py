7385ff5e103d47f013c057962449e0438dc93a45
Axel NIATO <axelniato@gmail.com>
2024-12-16 13:42:57 +0100

    Add multiprocessing with multiprocessing standard library

---
+++ Boss.py
@@ -0,0 +1,23 @@
+from QueueManager import QueueClient
+from task import Task
+
+
+class Boss(QueueClient):
+    def create_job(self, identifier=0):
+        task = Task(identifier)
+        self.task_queue.put(task)
+
+    def print_result(self):
+        result = self.result_queue.get()
+        print(result)
+
+
+if __name__ == "__main__":
+    boss = Boss()
+
+    for i in range(100):
+        boss.create_job(i)
+        print(f"job {i} create")
+
+    for i in range(100):
+        boss.print_result()
---
+++ Minion.py
@@ -0,0 +1,20 @@
+from QueueManager import QueueClient
+
+
+class Minion(QueueClient):
+    def __init__(self):
+        super().__init__()
+
+    def do_job(self):
+        task = self.task_queue.get()
+        id = task.get_identifier()
+        work = task.work()
+        print(f"job {id}, take by a minion. result {work}")
+        result = (id, work)
+        self.result_queue.put(result)
+
+
+if __name__ == "__main__":
+    minion = Minion()
+    while True:
+        minion.do_job()
